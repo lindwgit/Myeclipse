0ae5252b605b97cb334ac88487804aa56953c530
lindwgit <283431427@qq.com>
2019-03-04 20:56:28 +0800

    Maven处理第一个web

---
+++ src/test/java/com/dw/test/DBUtiltest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.dw.test;
+
+import org.junit.Test;
+
+/**
+ * @author dw
+ *
+ */
+public class DBUtiltest {
+	@Test
+	public void test(){
+		
+		System.out.println("dw");
+	}
+
+}
